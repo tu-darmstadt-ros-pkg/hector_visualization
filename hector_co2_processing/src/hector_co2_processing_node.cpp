dff86bc6c1a87e1b2e97f6cdc273b78eb6902b25
Kevin Daun <daun@sim.tu-darmstadt.de>
2021-09-28 08:48:45 +0200

    Ramp-based co2 detection.

--- hector_co2_processing/src/hector_co2_processing_node.cpp
+++ hector_co2_processing/src/hector_co2_processing_node.cpp
@@ -2,49 +2,37 @@
 #include <std_msgs/Float32.h>
 #include <std_msgs/Bool.h>
 
-ros::Publisher co2Pub;
 ros::Publisher detectedPub;
 
-float avg;
-
+float filtered_value;
 float threshold;
+float timeout_duration_sec;
+ros::Time last_ramp;
 
-float scale;
 
 void co2_callback(const std_msgs::Float32::ConstPtr &msg){
-    ROS_DEBUG("CO2 Callback");
-    ROS_DEBUG("avg_old: %f", avg);
-    avg = 0.9 * avg + 0.1 * msg->data;
-
-    ROS_DEBUG("avg: %f", avg);
-    double value = fabs(msg->data - avg);
-
-    ROS_DEBUG("Value: %f", value);
-    std_msgs::Float32 value_msg;
-    value_msg.data = scale * value;
-    co2Pub.publish(value_msg);
+    if(filtered_value == 0.f) filtered_value = msg->data;
+    filtered_value = 0.9 * filtered_value + 0.1 * msg->data;
+    float delta = msg->data - filtered_value;
+    ros::Time current_time = ros::Time::now();
+    if(delta > threshold) {
+      last_ramp = current_time;
+    }
 
     std_msgs::Bool bool_msg;
-    bool_msg.data = value > threshold;
+    bool_msg.data = (current_time - last_ramp).toSec() < timeout_duration_sec;
     detectedPub.publish(bool_msg);
-
 }
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "hector_co2_processing_node");
-
     ros::NodeHandle nh_;
-
-    avg = 0;
-    threshold = 50;
-    scale = 75.0;
-
-    ros::Subscriber co2Sub = nh_.subscribe("/co2", 100, &co2_callback);
-
-    co2Pub = nh_.advertise<std_msgs::Float32>("/processed_co2", 100);
-    detectedPub = nh_.advertise<std_msgs::Bool>("/co2detected", 100);
-
+    filtered_value = 0.f;
+    threshold = 50.f;
+    timeout_duration_sec = 2.0;
+    ros::Subscriber co2Sub = nh_.subscribe("/co2", 1, &co2_callback);
+    detectedPub = nh_.advertise<std_msgs::Bool>("/co2detected", 1);
     ros::spin();
     exit(0);
 }
